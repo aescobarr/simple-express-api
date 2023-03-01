12f394e60399ef85016559db746b338306dea792
aescobarr <agustiescobar@gmail.com>
2023-03-01 14:56:30 +0100

    Splash page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 
 var sumRouter = require('./routes/sum');
+var indexRouter = require('./routes/index');
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
+app.use('/', indexRouter);
 app.use('/sum', sumRouter);
 
 // catch 404 and forward to error handler
