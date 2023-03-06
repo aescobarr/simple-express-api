751f60448b55d78a4588770b5fbe3cc747e6701e
aescobarr <agustiescobar@gmail.com>
2023-03-06 10:19:26 +0100

    fail condition

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -4,4 +4,5 @@ test('test sum function', () => {
     expect( functions.sum(1,2) ).toBe(3);
     expect( functions.sum(-1,-1) ).toBe(-2);
     expect( functions.sum(-200,200) ).toBe(0);
+    expect( functions.sum(-200,200) ).toBe(1);
 });
