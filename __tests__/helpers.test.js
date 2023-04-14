01829626ee4b7504ba5b9417228a8aa868121f32
aescobarr <agustiescobar@gmail.com>
2023-04-14 16:09:13 +0200

    Removed fail condition

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -3,6 +3,5 @@ const functions = require('../helpers/functions');
 test('test sum function', () => {
     expect( functions.sum(1,2) ).toBe(3);
     expect( functions.sum(-1,-1) ).toBe(-2);
-    expect( functions.sum(-200,200) ).toBe(0);
-    expect( functions.sum(-200,200) ).toBe(1);
+    expect( functions.sum(-200,200) ).toBe(0);    
 });
